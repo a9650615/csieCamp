4e15894af65fb7f84628d70fd258aac37bb9707d
菜鳥祐 <a9650615@gmail.com>
2017-07-05 17:22:12 +0800

    CreateInput 內容送出清除功能

--- src/CreateInput.js
+++ src/CreateInput.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 
 export default class CreateInput extends Component {
+  state = {
+    text: ''
+  }
+
+  addTodo(e) {
+    if (e.keyCode == 13 && this.state.text != '') {
+      this.clear()
+    }
+  }
+
+  changText(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  clear() {
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <TextField
+       value={this.state.text}
        hintText="輸入新增事項"
        floatingLabelText="新增事項"
+       onChange={this.changText.bind(this)}
+       onKeyDown={this.addTodo.bind(this)}
        />
     )
   }
